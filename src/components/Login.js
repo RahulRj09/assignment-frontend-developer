dce3c9e7d4de9bbbb74c2f7e0088ed5a608a945c
RahulRj09 <rahul18@navgurukul.org>
2020-09-04 17:11:45 +0530

    changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,94 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { connect } from 'react-redux'
+// import { login } from '../../store'
+import { useHistory, Redirect, Link } from 'react-router-dom'
+import '../style/form.css'
+import '../style/form1.css'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
+import TextError from '../utils/TextError'
+import Alert from '@material-ui/lab/Alert';
+import '../style/login.css'
 
 function Login() {
+    let history = useHistory()
+
+    const initialValue = {
+        userName: "",
+        password: ""
+    }
+
+    const validationSchema = Yup.object({
+        userName: Yup.string().required("Required!"),
+        password: Yup.string().required("Required!")
+
+    })
+    const onSubmit = async values => {
+        // let data = await isAuthCall(values)
+        // localStorage.setItem("isAuth", true)
+        // history.push('/home')
+
+
+    }
+
+    let loginStatus = localStorage.getItem("isAuth")
+    // if (loginStatus === "true") {
+    //     return <Redirect to='/home' />
+    // }
     return (
         <div>
-            login
-        </div>
+            <section id="cover" className="min-vh-100" >
+                <div id="cover-caption">
+                    <div className="container-fluid">
+                        <div className="row text-white">
+                            <div className="col-xl-5 col-lg-8 col-md-8 col-sm-12 col-xs-12 col mx-auto form-4">
+                                <div className="template" style={{ color: 'black' }}>
+                                    <div className="card">
+                                        <div className="card-header" ><h1>Login</h1></div>
+                                        <div className="card-body" style={{ display: 'flex' }}>
+                                            <div style={{ width: '70%', marginLeft: '30%' }}>
+                                                <Formik initialValues={initialValue} validationSchema={validationSchema} onSubmit={onSubmit} >
+                                                    {
+                                                        formik => {
+                                                            return <Form>
+                                                                <div className="form-group">
+                                                                    <label htmlFor="userName">Username</label>
+                                                                    <Field type="text" name="userName" className="form-control" />
+                                                                    <ErrorMessage name="userName" component={TextError} />
+                                                                </div>
+                                                                <div className="form-group">
+                                                                    <label htmlFor="password">Password</label>
+                                                                    <Field type="password" name="password" className="form-control" />
+                                                                    <ErrorMessage name="password" component={TextError} />
+                                                                </div>
+                                                                {/* {
+
+                                                                    isAuth.error || isAuth.userData.status == false ?
+                                                                        <div>
+                                                                            <Alert severity="error">Invalid email/password</Alert>
+                                                                            <br />
+                                                                        </div>
+                                                                        : null
+                                                                } */}
+                                                                <br />
+                                                                <div style={{ display: "flex", float: "right", display: "inline", marginTop: "-8%" }}>
+                                                                    <button type='submit' className="btn btn-secondary" disabled={!formik.isValid}>Login</button>
+                                                                </div><br />
+                                                            </Form>
+                                                        }
+                                                    }
+                                                </Formik>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div >
+
     )
 }
 
